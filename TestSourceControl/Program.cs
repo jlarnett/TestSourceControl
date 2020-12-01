6697a2648864c39252d7bbab461b1cc5a3e9f63f
Johnny Arnett <im10g@hotmail.com>
2020-12-01 04:22:16 -0500

    test master branch visual studio raw features

--- TestSourceControl/Program.cs
+++ TestSourceControl/Program.cs
@@ -7,6 +7,8 @@ namespace TestSourceControl
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Yoo this is a test commit to test raw VS source control features");
         }
     }
 }
